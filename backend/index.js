37a474bb6184574ebed483a6144a983c1128fa75
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-10-05 00:04:07 +0530

    models created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ImageKit from "imagekit";
 import cors from "cors";
+import mongoose from "mongoose";
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -10,6 +11,15 @@ app.use(
     origin: process.env.CLIENT_URL,
   })
 );
+
+const connect = async () => {
+  try {
+    mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
@@ -23,5 +33,6 @@ app.get("/api/upload", (req, res) => {
 });
 
 app.listen(port, () => {
+  connect();
   console.log(`Server is runing on port number ${port}`);
 });
